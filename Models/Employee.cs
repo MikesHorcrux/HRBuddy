a426cf3665d9e64545300496cd6ff52df7b07b76
Mike Van Amburg <vanamburg.mv@gmail.com>
2019-10-20 14:54:45 -0600

    Added Employee data model

---
+++ Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRBuddy.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string FavColor { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string Shift { get; set; }
+        public string Manager { get; set; }
+        public int Employed { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+    }
+    //Add an img 
+}
+
